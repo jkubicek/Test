e1a9a454f5ae75003d20c920ffb60145983d9b26
Jim Kubicek <jkubicek@gmail.com>
2016-09-04 09:12:49 -0700

    Added a sample handlers

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,9 +1,23 @@
 import Kitura
+import HeliumLogger
+import SwiftyJSON
+
+HeliumLogger.use()
 
 let router = Router()
 
 router.get("/") { request, response, next in
   response.send("Hello, World!")
+  next()
+}
+
+router.get("json") { request, response, next in
+  response.send(json: JSON(["name": "Jim Kubicek", "message": "Hello, World"]))
+  next()
+}
+
+router.post("data/:id") { request, response, next in
+  response.send("Set data for ID: \(request.parameters["id"])\n\(try request.readString())")
   next()
 }
 
